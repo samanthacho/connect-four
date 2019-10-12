94961a62a9aaea3114437760c97d7130d67f938f
Srijon Saha <srijon365@gmail.com>
2019-10-12 09:19:59 -0700

    Fix broken win condition

--- Connect4.hs
+++ Connect4.hs
@@ -20,11 +20,12 @@ type Board = [[Char]]
 
 type ColPos = [Int]
 
+-- connect4 plays a turn of the game and updates the game baord
 connect4 :: Game
 connect4 player move (State board colPos)
     | isColFull move colPos = RetryTurn player (State board colPos)
     | isTie newColPos = EndOfGame 'T' (State newBoard newColPos)
-    | isWin player (move, newColPos!!move) newBoard = EndOfGame player (State newBoard newColPos)
+    | isWin player (move, colPos!!move) newBoard = EndOfGame player (State newBoard newColPos)
     | otherwise = ContinueGame (State newBoard newColPos)
     where
         (State newBoard newColPos) = updateBoard player move (State board colPos)
@@ -57,27 +58,29 @@ isTie (h:r) = h < 0 && isTie r
 
 isWin :: Player -> BoardPos -> Board -> Bool
 isWin player (x, y) board = 
-    (isWinVertically player (x2, y1) (x2, y3) board 0) ||
-    (isWinHorizontally player (x1, y2) (x3, y2) board 0) ||
-    (isWinTopLeftToBottomRight player (x1, y1) (x3, y3) board 0) ||
-    (isWinBottomLeftToTopRight player (x1, y3) (x3, y1) board 0)
+    (isWinVertically player (x, top) (x, bottom) board 0) ||
+    (isWinHorizontally player (left, y) (right, y) board 0) ||
+    (isWinTopLeftToBottomRight player (tlx, tly) (brx, bry) board 0) ||
+    (isWinBottomLeftToTopRight player (blx, bly) (trx, try) board 0)
     where
-        x1 = max (x-3) 0; x2 = x; x3 = min(x+3) 6
-        y1 = max (y-3) 0; y2 = y; y3 = min(y+3) 5
+        left = max (x-3) 0; right = min (x+3) 6
+        top = max (y-3) 0; bottom = min (y+3) 5
+        (tlx, tly) = findTopLeft (x, y) 3; (trx, try) = findTopRight (x, y) 3
+        (blx, bly) = findBottomLeft (x, y) 3; (brx, bry) = findBottomRight (x, y) 3
 
 isWinVertically :: Player -> BoardPos -> BoardPos -> Board -> Int -> Bool
 isWinVertically player (x1, y1) (x2, y2) board acc
     | acc >= 4 = True
-    | x1 > x2 || y1 > y2 = False
-    | board!!y1!!x1 == player = isWinVertically player (x1+1, y1) (x2, y2) board (acc+1)
-    | otherwise = isWinVertically player (x1+1, y1) (x2, y2) board 0
+    | y1 > y2 = False
+    | board!!y1!!x1 == player = isWinVertically player (x1, y1+1) (x2, y2) board (acc+1)
+    | otherwise = isWinVertically player (x1, y1+1) (x2, y2) board 0
 
 isWinHorizontally :: Player -> BoardPos -> BoardPos -> Board -> Int -> Bool
 isWinHorizontally player (x1, y1) (x2, y2) board acc
     | acc >= 4 = True
-    | x1 > x2 || y1 > y2 = False
-    | board!!y1!!x1 == player = isWinHorizontally player (x1, y1+1) (x2, y2) board (acc+1)
-    | otherwise = isWinHorizontally player (x1, y1+1) (x2, y2) board 0
+    | x1 > x2 = False
+    | board!!y1!!x1 == player = isWinHorizontally player (x1+1, y1) (x2, y2) board (acc+1)
+    | otherwise = isWinHorizontally player (x1+1, y1) (x2, y2) board 0
 
 isWinTopLeftToBottomRight :: Player -> BoardPos -> BoardPos -> Board -> Int -> Bool
 isWinTopLeftToBottomRight player (x1, y1) (x2, y2) board acc
@@ -89,9 +92,29 @@ isWinTopLeftToBottomRight player (x1, y1) (x2, y2) board acc
 isWinBottomLeftToTopRight :: Player -> BoardPos -> BoardPos -> Board -> Int -> Bool
 isWinBottomLeftToTopRight player (x1, y1) (x2, y2) board acc
     | acc >= 4 = True
-    | x1 > x2 || y1 > y2 = False
+    | x1 > x2 || y1 < y2 = False
     | board!!y1!!x1 == player = isWinBottomLeftToTopRight player (x1+1, y1-1) (x2, y2) board (acc+1)
     | otherwise = isWinBottomLeftToTopRight player (x1+1, y1-1) (x2, y2) board 0
+
+findTopLeft (x, y) dist
+    | dist == 0 = (x, y)
+    | x == 0 || y == 0 = (x, y)
+    | otherwise = findTopLeft (x-1, y-1) (dist-1)
+
+findTopRight (x, y) dist
+    | dist == 0 = (x, y)
+    | x == 6 || y == 0 = (x, y)
+    | otherwise = findTopRight (x+1, y-1) (dist-1)
+
+findBottomLeft (x, y) dist
+    | dist == 0 = (x, y)
+    | x == 0 || y == 5 = (x, y)
+    | otherwise = findBottomLeft (x-1, y+1) (dist-1)
+
+findBottomRight (x, y) dist
+    | dist == 0 = (x, y)
+    | x == 6 || y == 5 = (x, y)
+    | otherwise = findBottomRight (x+1, y+1) (dist-1)
 
 -- updateList i newVal lst replaces the element at index i of lst with newVal
 updateList :: Int -> t -> [t] -> [t]
