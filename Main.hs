58c2f365a8cdaed5a7fa954bc2284fe85a245f13
Samantha Cho <samantha.cho@alumni.ubc.ca>
2019-10-18 13:49:05 -0700

    updated comments

--- Main.hs
+++ Main.hs
@@ -54,6 +54,8 @@ playTurn isAi player (State board colPos lastMove) level =
                 let z = input - 1
                 return (connect4 player z (State board colPos lastMove))
 
+-- atRandIndex l takes a list and returns a random
+-- element of that list
 atRandIndex :: [a] -> IO a
 atRandIndex l = do
     i <- randomRIO (0, length l - 1)
@@ -83,48 +85,69 @@ endTurn (RetryTurn player (State board colPos lastMove)) =
         putStrLn "That column is full. Try another."
         return (False, True, (State board colPos lastMove))
 
+-- createAiMove level lastMove creates the CPU player move for levels 0 and 1
 createAiMove :: Int -> Int -> IO Int
 createAiMove level lastMove =
     do
         if level == 0 then do
+            -- if the level is easiest, choose a random column
             x <- atRandIndex column_numbers
+            -- check to make sure the column is valid
             if isValidGameInput x
                 then
+                    -- return the column
                     return (digitToInt x)
+                -- grab a different column
                 else do createAiMove level lastMove
         else do
+            -- if the level is 1, play an adjacent column
             x <- atRandIndex [intToDigit lastMove, intToDigit (lastMove + 1), intToDigit (lastMove + 2)]
+            -- if this column is valid, then use it
             if isValidGameInput x
                 then
                     return (digitToInt x)
+                -- if not, grab a random column
                 else do createAiMove 0 lastMove
 
+-- moveChecker (h:t) player state
+-- checks if there is a CPU or player win. Return a non-existent column
+-- if neither exist
 moveChecker :: [Int] -> Player -> State -> IO Int
 moveChecker [] _ (State board colPos lastMove) =
     do
+        -- return non-existent column
         return 8
 moveChecker (h:t) player (State board colPos lastMove) =
     do
         -- check CPU win
         if (colPos!!h >= 0) then do
+            -- update board for current column in question played by CPU
             let (State newBoard newColPos lastMove) = updateBoard p1 h (State board colPos lastMove)
+            -- check if the CPU has won
             if isWin player (h, ((newColPos!!h) + 1)) newBoard
                 then do
+                    -- return this column
                     return (h + 1)
+                -- this column is not a win for the CPU
                 else do
-                    -- check player win
+                    -- update board for current column in question played by player
                     let (State newBoard newColPos lastMove) = updateBoard p2 h (State board colPos lastMove)
+                    -- check if player has won
                     if isWin (if player == p1 then p2 else p1) (h, ((newColPos!!h) + 1)) newBoard
                         then do
+                            -- play this column
                             return (h+1)
                         else do
+                            -- repeat until all columns checked
                             moveChecker t player (State board colPos lastMove)
             else do
                 moveChecker t player (State board colPos lastMove)
 
+-- purifierFunction x purifies an IO int, and subtract one
 purifierFunction :: Int -> Int
 purifierFunction x = x - 1
 
+-- zeroPurifier x purifies an IO int
 zeroPurifier :: Int -> Int
 zeroPurifier x = x + 0
 
@@ -165,20 +188,26 @@ main =
         putStrLn "Press c for single player, any other key for multi player"
         c <- getChar
         if (c == 'c')
+            -- if player chooses CPU single player
+            -- prompt them for level input
             then do
                 putStrLn "\n" -- extra empty line for sexiness
                 putStrLn "Computer is player 'X'. You are Player 0."
                 putStrLn "Choose either 0, 1, or 2 for difficulty"
                 l <- getChar
+                -- check to make sure the input is a level
                 if isValidLevel l
                     then do
+                        -- start the game with given level
                         putStrLn "\n" -- extra empty line for sexiness
                         play True False p1 initState (digitToInt l)
                     else do
+                        -- take user back to the main page
                         putStrLn "\n" -- extra empty line for sexiness
                         putStrLn "Not a valid level input. Redirecting to main."
                         main
         else do
+            -- players have chosen pvp. start the game.
             putStrLn "Player 1 is 'X' and Player 2 is 'O'"
             putStrLn "\n" -- extra empty line for sexiness
             play False False p1 initState 0
